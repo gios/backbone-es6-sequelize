a174bd7338f2c050163b31941a7701bcf0e3282a
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2015-06-22 13:27:19 +0300

    Remove unused code

--- app/stores/todoStore.js
+++ app/stores/todoStore.js
@@ -1,7 +1,7 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher');
-var appConstants = require('../constants/appConstants');
-var objectAssign = require('react/lib/Object.assign');
-var EventEmitter = require('events').EventEmitter;
+var AppDispatcher = require('../dispatcher/AppDispatcher'),
+  appConstants = require('../constants/appConstants'),
+  objectAssign = require('react/lib/Object.assign'),
+  EventEmitter = require('events').EventEmitter;
 
 var CHANGE_EVENT = 'change';
 
@@ -9,29 +9,29 @@ var _store = {
   list: []
 };
 
-var addItem = function(item){
+var addItem = function(item) {
   _store.list.push(item);
 };
 
-var removeItem = function(index){
+var removeItem = function(index) {
   _store.list.splice(index, 1);
-}
+};
 
 var todoStore = objectAssign({}, EventEmitter.prototype, {
-  addChangeListener: function(cb){
+  addChangeListener: function(cb) {
     this.on(CHANGE_EVENT, cb);
   },
-  removeChangeListener: function(cb){
+  removeChangeListener: function(cb) {
     this.removeListener(CHANGE_EVENT, cb);
   },
-  getList: function(){
+  getList: function() {
     return _store.list;
   },
 });
 
-AppDispatcher.register(function(payload){
+AppDispatcher.register(function(payload) {
   var action = payload.action;
-  switch(action.actionType){
+  switch (action.actionType) {
     case appConstants.ADD_ITEM:
       addItem(action.data);
       todoStore.emit(CHANGE_EVENT);
@@ -45,4 +45,4 @@ AppDispatcher.register(function(payload){
   }
 });
 
-module.exports = todoStore;
+export default todoStore;
--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,7 @@ gulp.task("webpack", function(callback) {
 
     webpack(webpackConfig, function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
+        gutil.log("[webpack]", stats.toString());
         callback();
     });
 });
